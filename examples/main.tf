60500ffb2c1ac5f3b88169ceb111a7fe594b5ffd
Matheus Braga <matheus@gocache.com.br>
2021-08-27 16:12:43 -0300

    examples

---
+++ examples/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    gocache = {
+      source = "gocache.com.br/gocachebr/gocache"
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "gocache" {
+  api_key = "5e28d7603418957c"
+}
+
+resource "gocache_domain" "example" {
+  domain = "example.com.br" 
+  waf_status = true
+  cdn_mode = "ns"
+}
+
+
+resource "gocache_record" "www" {
+  domain = gocache_domain.example.domain
+  name = "www"
+  type = "A"
+  proxied = true
+  ttl = 60
+  content = "192.168.200.100"
+}
+
+resource "gocache_record" "teste" {
+  domain = gocache_domain.example.domain
+  name = "@"
+  type = "A"
+  proxied = true
+  ttl = 60
+  content = "192.168.200.101"
+}
+
+resource "gocache_record" "bolo" {
+  domain = gocache_domain.example.domain
+  name = "test"
+  type = "CNAME"
+  proxied = false
+  ttl = 60
+  content = "www.example.com.br"
+}
+
+output "example_output"{
+  value = gocache_domain.example 
+}
